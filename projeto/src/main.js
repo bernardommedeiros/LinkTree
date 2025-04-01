4872a140f489ed5bfc38613fde6bfee43d8f918a
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-04-01 17:32:37 -0300

    padronização icons,  ancoragem dos sites e logica de baixar Pdfs

--- projeto/src/main.js
+++ projeto/src/main.js
@@ -1,11 +1,10 @@
-const popup = document.getElementById("gmailCard");
+document.getElementById('curriculo_download').addEventListener('click', function() {
 
-function gmailPopup(open) {
-  if (open) {
-    popup.classList.remove("hidden");
-    popup.classList.add("block");  // Torna o popup visível
-  } else {
-    popup.classList.add("hidden");
-    popup.classList.remove("block");  // Esconde o popup
-  }
-}
+    var pdfRoute = '../components/curriculo.pdf';
+    
+    // cria um link temporário para download
+    var link = document.createElement('a');
+    link.href = pdfRoute;
+    link.download = 'curriculo.pdf'; // Nome do arquivo ao ser baixado
+    link.click(); // Simula o clique no link para iniciar o download
+});
